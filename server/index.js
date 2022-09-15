41ed6246a32234795b184fd2fa64e69c0411a4d7
Tanner White <tannerjwhite618@gmail.com>
2022-09-15 16:50:54 -0500

    add table creation to database index file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
-// CREATE TABLE <tableName> (
-//   id SERIAL PRIMARY KEY,
-//   name VARCHAR NOT NULL,
-//   price NUMERIC(10,2) DEFAULT 0
-//   );
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+console.log(`LISTENING ON PORT ${PORT}`);
