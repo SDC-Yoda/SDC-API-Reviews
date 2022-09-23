5399cfb565181f7fe43ebe8afc1e540ec5ce0822
Ubuntu <ubuntu@ip-172-31-36-124.us-east-2.compute.internal>
2022-09-23 17:45:20 +0000

    Updates server configuration for deployed version

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/loaderio-956e4ceecb4e59cbe5174f246347115d.txt", (req, res) => {
+ return res.status(200).send("loaderio-956e4ceecb4e59cbe5174f246347115d")
+})
+
 app.get('/reviews', (req, res) => {
   return reviewsCntl.getReviews(req, res)
 })
